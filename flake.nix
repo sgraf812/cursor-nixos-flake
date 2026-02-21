010518ecbf4184b11972278f9f2dd2dfaacb2029
GitHub Action <action@github.com>
2026-02-21 12:20:18 +0000

    Update Cursor to version 2.5.20

--- flake.nix
+++ flake.nix
@@ -106,9 +106,9 @@
       packages.${system} = {
         default = self.packages.${system}.cursor;
         cursor = buildCursor {
-          version = "2.5.17";
-          url = "https://downloads.cursor.com/production/7b98dcb824ea96c9c62362a5e80dbf0d1aae4775/linux/x64/Cursor-2.5.17-x86_64.AppImage";
-          sha256 = "0a22m7qw9y7c1qlvnjjbiz290fr051l98dij2z3cal0l35znlidm";  # Will be updated by GitHub Actions
+          version = "2.5.20";
+          url = "https://downloads.cursor.com/production/511523af765daeb1fa69500ab0df5b6524424612/linux/x64/Cursor-2.5.20-x86_64.AppImage";
+          sha256 = "1vrwqyz8qg442kixj48fa47bqs6y6zyp5s5hid7z8pyf678y7jvj";  # Will be updated by GitHub Actions
         };
       };
 
